78b88d199980bec940ea45425c3e351080e34065
Sharifah Munira <shifa@myorange.my>
2018-12-24 13:23:31 +0800

    user database models

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
+
+class UserProfiles(AbstracBaseUser, PermissionsMixin):
+        """Respents a "user profiles" inside our system"""
+
+        email = models.EmailField(max_lenght=255, unique=True)
+        name = models.CharField(max_lenght=255)
+        is_active = models.BooleanField(default=True)
+        is_staff = models.BooleanField(default=False)
+
+        objects = UserProfileManager()
+
+        USERNAME_FIELD = 'email'
+        REQUIRED_FIELD = ['name']
+
+        def get_full_name(self):
+            """Used to get a user full name"""
+
+            return self.name
+
+        def get_short_name(self):
+            """Used to get a user short name"""
+
+            return self.name
+
+        def __str__(self):
+            """Django uses this when it need to convert the object to string"""
+
+            return self.email
